877eef88b46511ce4f6982576669972271c1390d
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 12:46:02 -0700

    added DocStrings to all functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,55 @@
 def add(num1, num2):
+    """addition function
+
+    add num1 and num2 together and pass result back
+    """
     return num1 + num2
 
 def subtract(num1, num2):
+    """subtraction function
+
+    subtract num2 from num1 and pass result back
+    """
     return num1 - num2
 
 def multiply(num1, num2):
+    """multiply function
+
+    multiply num1 by num2 and pass result back
+    """
     return num1 * num2
 
 def divide(num1, num2):
-    return float(num1) / float(num2)
+    """divide function
+
+    divide num1 by num2 and pass result back
+    """
+    return float(num1) / num2
 
 def square(num1):
+    """square function
+
+    square num1 and pass result back
+    """
     return num1**2
 
 def cube(num1):
+    """cube function
+
+    cube num1 and pass result back
+    """
     return num1**3
 
 def power(num1, num2):
+    """power function
+
+    num1 to the power of num2 and pass result back
+    """
     return num1**num2
 
 def mod(num1, num2):
+    """modulus function
+
+    divide num1 by num2 and pass back remainder
+    """
     return num1 % num2
